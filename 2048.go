c321848221904cbeddf44caca9930fa9d4403171
xuzhenglun <xuzhenglun@gmail.com>
2015-09-24 10:20:18 +0800

    initial commit

---
+++ 2048.go
@@ -0,0 +1,216 @@
+package main
+
+import (
+	"fmt"
+	"github.com/nsf/termbox-go"
+	"github.com/xuzhenglun/2048-Go/martix"
+	"strconv"
+)
+
+const MAX_LEN int = 4
+
+var step int
+
+type Go2048 struct {
+	martix.Martix
+}
+
+func (this Go2048) GoUp() bool {
+	this.Left90()
+	change := this.Combin()
+	this.Left90()
+	return change
+}
+
+func (this Go2048) GoDown() bool {
+	this.Right90()
+	change := this.Combin()
+	this.Right90()
+	return change
+}
+
+func (this Go2048) GoLeft() bool {
+	change := this.Combin()
+	return change
+}
+
+func (this Go2048) GoRight() bool {
+	this.Mirror()
+	change := this.Combin()
+	this.Mirror()
+	return change
+}
+
+func (this Go2048) CheckWinOrLose() bool {
+	for x, row := range this.Martix {
+		for y, _ := range row {
+			if this.Martix[x][y] == 0 {
+				return true
+				//true = Have not been dead yet
+			}
+		}
+	}
+	return false
+	//false = Lose
+}
+
+func (this Go2048) Init_termbox(x, y int) error {
+	fg := termbox.ColorYellow
+	bg := termbox.ColorBlack
+	err := termbox.Clear(fg, bg)
+	if err != nil {
+		return err
+	}
+	str := "Enter: restart game"
+	for n, c := range str {
+		termbox.SetCell(x+n, y-1, c, fg, bg)
+	}
+
+	str = "ESC: quit game" + "  Step: " + strconv.Itoa(step)
+	for n, c := range str {
+		termbox.SetCell(x+n, y-2, c, fg, bg)
+	}
+
+	str = "Play with Arrow Key"
+	for n, c := range str {
+		termbox.SetCell(x+n, y-3, c, fg, bg)
+	}
+
+	fg = termbox.ColorBlack
+	bg = termbox.ColorGreen
+	for i := 0; i <= len(this.Martix); i++ {
+		for t := 0; t < 6*len(this.Martix); t++ {
+			if t%6 != 0 {
+				termbox.SetCell(x+t, y+i*2, '-', fg, bg)
+			}
+		}
+		for t := 0; t <= 2*len(this.Martix); t++ {
+			if t%2 == 0 {
+				termbox.SetCell(x+i*6, y+t, '+', fg, bg)
+			} else {
+				termbox.SetCell(x+i*6, y+t, '|', fg, bg)
+			}
+		}
+	}
+
+	fg = termbox.ColorYellow
+	bg = termbox.ColorBlack
+	for i, row := range this.Martix {
+		for j, _ := range row {
+			if this.Martix[i][j] > 0 {
+				str := fmt.Sprint(this.Martix[i][j])
+				for n, char := range str {
+					termbox.SetCell(x+j*6+1+n, y+i*2+1, char, fg, bg)
+				}
+			}
+		}
+	}
+	return termbox.Flush()
+}
+
+func converPrintStr(x, y int, str string, fg, bg termbox.Attribute) error {
+	xx := x
+	for n, c := range str {
+		if c == '\n' {
+			y++
+			xx = x - n - 1
+		}
+		termbox.SetCell(xx+n, y, c, fg, bg)
+	}
+	return termbox.Flush()
+}
+
+func (t *Go2048) ListernKey() chan termbox.Event {
+	//ev := termbox.PollEvent()
+	event_queue := make(chan termbox.Event)
+	go func() {
+		for {
+			event_queue <- termbox.PollEvent() // 开始监听键盘事件
+		}
+	}()
+	return event_queue
+}
+
+func (t *Go2048) ActionAndReturnKey(event_queue chan termbox.Event) termbox.Key {
+	for {
+		ev := <-event_queue
+		changed := false
+
+		switch ev.Type {
+		case termbox.EventKey:
+			switch ev.Key {
+			case termbox.KeyArrowUp:
+				changed = t.GoUp()
+			case termbox.KeyArrowDown:
+				changed = t.GoDown()
+			case termbox.KeyArrowLeft:
+				changed = t.GoLeft()
+			case termbox.KeyArrowRight:
+				changed = t.GoRight()
+			case termbox.KeyEsc, termbox.KeyEnter:
+				changed = true
+			default:
+				changed = false
+			}
+
+			// 如果元素的值没有任何更改，则从新开始循环
+			if !changed && t.CheckWinOrLose() {
+				continue
+			}
+
+		case termbox.EventResize:
+			x, y := termbox.Size()
+			t.Init_termbox(x/2-10, y/2-4)
+			continue
+		case termbox.EventError:
+			panic(ev.Err)
+		}
+		return ev.Key
+	}
+}
+
+func main() {
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
+	x, y := termbox.Size()
+
+	martix.Init()
+	var t Go2048
+	t.Martix, _ = martix.Init_martix(MAX_LEN)
+	t.AddNum()
+	step = 0
+	ch := t.ListernKey()
+	defer close(ch)
+
+	for {
+		t.Init_termbox(x/2-10, y/2-4)
+
+		key := t.ActionAndReturnKey(ch)
+
+		if t.CheckWinOrLose() == false {
+			str := "Lose!"
+			strlen := len(str)
+			converPrintStr(x/2-strlen/2, y/2, str, termbox.ColorBlack, termbox.ColorRed)
+			for {
+				key = t.ActionAndReturnKey(ch)
+				if key == termbox.KeyEnter || key == termbox.KeyEsc {
+					break
+				}
+			}
+		}
+
+		if key == termbox.KeyEnter {
+			t.Martix, _ = martix.Init_martix(MAX_LEN)
+			step = -1
+		}
+		if key == termbox.KeyEsc {
+			return
+		}
+
+		step++
+		t.AddNum()
+	}
+}
---
+++ martix/martix.go
@@ -0,0 +1,164 @@
+package martix
+
+import (
+	//"fmt"
+	"math/rand"
+	//"runtime"
+	"time"
+)
+
+type row []int
+type Martix []row
+
+func Init_martix(l int) (Martix, error) {
+	martix := make([]row, l)
+	for index, _ := range martix {
+		martix[index] = make(row, l)
+	}
+	return martix, nil
+}
+
+func (this Martix) Mirror() {
+	for index, _ := range this {
+		for i := 0; i < len(this[index])/2; i++ {
+			swap(&this[index][i], &this[index][len(this[index])-i-1])
+		}
+	}
+}
+
+func (this Martix) Left90() {
+	for x, row := range this {
+		for y, _ := range row {
+			if x > y {
+				swap(&this[x][y], &this[y][x])
+			}
+		}
+	}
+}
+
+func (this Martix) Right90() {
+	for x, row := range this {
+		for y, _ := range row {
+			if x+y < len(this)-1 {
+				swap(&this[x][y], &this[len(this)-y-1][len(this)-x-1])
+			}
+		}
+	}
+}
+
+func (this Martix) Combin() bool {
+	ch := make(chan bool, len(this))
+	defer close(ch)
+	change := false
+	for _, row := range this {
+		go row.combin_row(ch)
+	}
+	for i := 0; i < len(this); i++ {
+		if <-ch == true {
+			change = true
+		}
+	}
+	return change
+}
+
+func (this row) combin_row(ch chan bool) {
+	change := false
+	table := make([]int, 0)
+	for i, node := range this {
+		if node != 0 {
+			table = append(table, i)
+		}
+	}
+	for {
+		flag := len(table)
+		for num, _ := range table {
+			if len(table) == 1 || num == len(table)-1 {
+				break
+			}
+			if this[table[num]] == this[table[num+1]] {
+				this[table[num]] *= 2
+				this[table[num+1]] = 0
+				table = append(table[:num+1], table[num+2:]...)
+				change = true
+				break
+			}
+		}
+		if len(table) == flag {
+			break
+		}
+	}
+	i := 0
+	for num, _ := range table {
+		this[i] = this[table[num]]
+		if i != table[num] {
+			change = true
+			this[table[num]] = 0
+		}
+		i++
+	}
+	ch <- change
+}
+
+//func main() {
+//runtime.GOMAXPROCS(runtime.NumCPU())
+//a, _ := Init_martix(4)
+//a[0][0] = 2
+//a[0][1] = 2
+//a[0][2] = 4
+//a[0][3] = 8
+//a[1][0] = 2
+//a[1][1] = 6
+//a[1][2] = 7
+//a[1][3] = 8
+//a[2][0] = 4
+//a[2][1] = 4
+//a[2][2] = 11
+//a[2][3] = 12
+//a[3][0] = 8
+//a[3][1] = 14
+//a[3][2] = 15
+//a[3][3] = 16
+//fmt.Println(a)
+//a.Left90()
+//fmt.Println(a)
+//a.Right90()
+//fmt.Println(a)
+//a.Combin()
+//fmt.Println(a)
+//a.AddNum()
+//fmt.Println(a)
+//}
+
+func swap(a, b *int) {
+	tmp := *b
+	*b = *a
+	*a = tmp
+}
+
+func (this Martix) AddNum() {
+	table := make([]int, 0)
+	for x, row := range this {
+		for y, _ := range row {
+			if this[x][y] == 0 {
+				table = append(table, x*len(this)+y)
+			}
+		}
+	}
+
+	ch := make(chan bool, 2)
+	defer close(ch)
+	for i := 0; i < 2; i++ {
+		go func() {
+			ran := rand.Intn(len(table))
+			this[table[ran]/len(this)][table[ran]%len(this)] = 2 << (rand.Uint32() % 2)
+			ch <- true
+		}()
+	}
+	for i := 0; i < 2; i++ {
+		<-ch
+	}
+}
+
+func Init() {
+	rand.Seed(time.Now().UnixNano())
+}
